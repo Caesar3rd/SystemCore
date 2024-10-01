c9d72857a94e05182cd25e93bc4bbbcbb79d0f5b
Caesar_3 <129692113+angikoko@users.noreply.github.com>
2024-10-01 17:28:22 +0200

    Updated SystemCore.java
    Added onEnable and onDisable listeners.

--- src/main/java/McSystem/systemCore/SystemCore.java
+++ src/main/java/McSystem/systemCore/SystemCore.java
@@ -2,16 +2,17 @@ package McSystem.systemCore;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SystemCore extends JavaPlugin {
+public final class  SystemCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("System Core has started!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("System Core has stopped!");
     }
 }
