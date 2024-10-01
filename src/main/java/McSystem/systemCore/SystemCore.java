351d91dac1ac55111fdca00c89c5a0e166a9dd17
Caesar_3 <129692113+Caesar3rd@users.noreply.github.com>
2024-10-01 19:57:30 +0200

    Update SystemCore.java
    Added the following commands:
    - /die

--- src/main/java/McSystem/systemCore/SystemCore.java
+++ src/main/java/McSystem/systemCore/SystemCore.java
@@ -1,5 +1,6 @@
 package McSystem.systemCore;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class  SystemCore extends JavaPlugin {
@@ -8,6 +9,17 @@ public final class  SystemCore extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         System.out.println("System Core has started!");
+    }
+
+    @Override
+    public  boolean onCommand(org.bukkit.command.CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        // command: /die, it kills the player and send him a message
+        if (command.getName().equalsIgnoreCase("die")) {
+            Player p = (Player) sender;
+            p.setHealth(0);
+            sender.sendMessage("Hai usato i comandi per killarti!");
+        }
+            return true;
     }
 
     @Override
