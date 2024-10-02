4bae5ef48164eb43ae3894f1569f76bd8dd22e3d
Caesar_3 <129692113+Caesar3rd@users.noreply.github.com>
2024-10-02 18:25:57 +0200

    Update SystemCore.java

--- src/main/java/McSystem/systemCore/SystemCore.java
+++ src/main/java/McSystem/systemCore/SystemCore.java
@@ -1,5 +1,6 @@
 package McSystem.systemCore;
 
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,13 +14,15 @@ public final class  SystemCore extends JavaPlugin {
 
     @Override
     public  boolean onCommand(org.bukkit.command.CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-        // command: /die, it kills the player and send him a message
+        // command: /kill, it kills the player and send him a message
         if (command.getName().equalsIgnoreCase("die")) {
             Player p = (Player) sender;
             p.setHealth(0);
             sender.sendMessage("Hai usato i comandi per killarti!");
-        }
-            return true;
+        }else if(sender instanceof ConsoleCommandSender){
+            System.out.println("The command was runned by Console");
+        };
+        return true;
     }
 
     @Override
