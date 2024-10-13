e3a285ee62d160a937f26cb1ec8688d0da281a9f
Caesar_3 <129692113+Caesar3rd@users.noreply.github.com>
2024-10-13 14:32:17 +0200

    Update SystemCore.java

--- src/main/java/McSystem/systemCore/SystemCore.java
+++ src/main/java/McSystem/systemCore/SystemCore.java
@@ -1,29 +1,21 @@
 package McSystem.systemCore;
-
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
+import McSystem.systemCore.Commands.Kill;
+import McSystem.systemCore.Commands.godcommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class  SystemCore extends JavaPlugin {
+
+
+public final class SystemCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("System Core has started!");
+
+        getCommand("kill").setExecutor(new Kill());
+        getCommand("Godcommand").setExecutor(new godcommand());
     }
 
-    @Override
-    public  boolean onCommand(org.bukkit.command.CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-        // command: /kill, it kills the player and send him a message
-        if (command.getName().equalsIgnoreCase("kill")) {
-            Player p = (Player) sender;
-            p.setHealth(0);
-            sender.sendMessage("Hai usato i comandi per killarti!");
-        }else if(sender instanceof ConsoleCommandSender){
-            System.out.println("The command was runned by Console");
-        };
-        return true;
-    }
 
     @Override
     public void onDisable() {
